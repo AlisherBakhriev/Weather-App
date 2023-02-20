22efb56b81bc9029c91a41b056da3ae7fb25accb
alisher <chexramchik@gmail.com>
2023-02-20 20:32:51 +0500

    g

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let cityRef = document.getElementById("city");
 let getWeather = () => {
     let cityValue = cityRef.value;
     if (cityValue.length == 0) {
-        result.innerHTML = `<h3 class="msg">PLEASE ENTER CITY NAME.</h3>`;
+        result.innerHTML = `<h3 class="msg">PLEASE ENTER CITY NAME. </h3>`;
     }
     else {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
@@ -21,22 +21,22 @@ let getWeather = () => {
                 console.log(data.main.temp_min);
                 console.log(data.main.temp_max);
                 result.innerHTML = `
-        <h2>${data.name}</h2>
-        <h4 class="weather">${data.weather[0].main}</h4>
-        <h4 class="desc">${data.weather[0].description}</h4>
-        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
-        <h1>${data.main.temp} &#176;</h1>
-        <div class="temp-container">
-            <div>
-                <h4 class="title">lowest temperature</h4>
-                <h4 class="temp">${data.main.temp_min}&#176;</h4>
-            </div>
-            <div>
-                <h4 class="title">highest temperature</h4>
-                <h4 class="temp">${data.main.temp_max}&#176;</h4>
-            </div>
-        </div>
-        `;
+                    <h2>${data.name}</h2>
+                    <h4 class="weather">${data.weather[0].main}</h4>
+                    <h4 class="desc">${data.weather[0].description}</h4>
+                    <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+                    <h1>${data.main.temp} &#176;</h1>
+                    <div class="temp-container">
+                    <div>
+                        <h4 class="title">lowest temperature</h4>
+                        <h4 class="temp">${data.main.temp_min}&#176;</h4>
+                    </div>
+                    <div>
+                        <h4 class="title">highest temperature</h4>
+                        <h4 class="temp">${data.main.temp_max}&#176;</h4>
+                    </div>
+                </div>
+                 `;
             })
             .catch(() => {
                 result.innerHTML = `<h3 class="msg">City not found</h3>`;
